e6c9f8da32708fd968779ce71fda33e5c78f2c1f
sarabmoraes1 <sarabmoraes1@gmail.com>
2021-06-17 04:59:55 -0300

    Ajustando Idade
    Ajustando Idade

--- Aula10.html/Aula12.js/ex011.js
+++ Aula10.html/Aula12.js/ex011.js
@@ -2,7 +2,9 @@ var idade = 22
 if (idade <16) {
     console.log('Não vota')
 } else {
-    if (idade >= 16 && idade < 18){
+    if (idade < 18 || idade > 65) {
         console.log('voto opcional')
+    } else {
+        console.log ('voto obrigatório')
     }
 }
---
+++ Aula10.html/Aula12.js/ex012.js
@@ -0,0 +1,9 @@
+var hora = 8
+console.log(`agora são extamente ${hora} horas`)
+if (hora < 12 ){
+    console.log ('Bom dia')
+} else if (hora <= 18){
+    console.log('Boa tarde')
+} else {
+    console.log ('Boa noite')
+}
