dc8d491f420aba6f2ba1f1dda3603e310d6eeab1
sarabmoraes1 <sarabmoraes1@gmail.com>
2021-07-01 22:50:46 -0300

    Ajustes
    Ajustes

---
+++ Aula12ex/ex014/script.js
@@ -0,0 +1,10 @@
+function carregar(){
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas.`
+if(hora >=)
+
+}
+
