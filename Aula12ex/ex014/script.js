fed61e13bba1f6a94becdbc383fdd6122a70c98e
sarabmoraes1 <sarabmoraes1@gmail.com>
2021-07-05 20:58:06 -0300

    NewDate
    NewDate

--- Aula12ex/ex014/script.js
+++ Aula12ex/ex014/script.js
@@ -2,9 +2,19 @@ function carregar(){
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date()
-var hora = data.getHours()
+//var hora = data.getHours()
+var hora = 22
 msg.innerHTML = `Agora são ${hora} horas.`
-if(hora >=)
+if(hora >= 0 && hora < 12){
+    //bom dia!
+    img.src ='fotomanha.png'
+} else if (hora >= 12 && hora < 18){
+    //boa tarde!
+    img.src = 'fototarde.png'
+} else {
+    //boa noite!
+    img.src = 'fotonoite.png'
+}
 
 }
 
